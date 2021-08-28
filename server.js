b348b3a0c938d4d16a4278f97f4be48e82aa3883
Melinda Ivanov <jns.melinda@gmail.com>
2021-08-28 16:25:06 -0700

    fixing path

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/api', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public'));
+  res.sendFile(path.join(__dirname, 'public/'));
 });
 
 app.listen(PORT, () => {
