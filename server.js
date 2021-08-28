957326f533ece8ffd74c06ad62f1b4357642c58d
Melinda Ivanov <jns.melinda@gmail.com>
2021-08-28 16:15:50 -0700

    route update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080;
-const data = require('./data.json');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -9,15 +8,11 @@ app.use(express.json());
 app.use(express.static('public'))
 
 app.get('/api', (req, res) => {
-  res.send(data)
-})
-
-app.post('/api', function (req, res) {
   res.json(req.body);
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, 'public'));
 });
 
 app.listen(PORT, () => {
